1252e7321fafc83faeb80f3a300835da4715401d
Luiz Philippe <luiz@phari.solutions>
2018-06-15 16:00:47 -0300

    Styling adjustments

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 
 class Options extends Component {
 
-  addTask
-
   constructor(props) {
     super(props)
     // Set callback to task creation
@@ -17,9 +15,25 @@ class Options extends Component {
         description: '',
         priority: 'Ultra Low',
         stage_id: 0,
-        time: '2 Days'
+        time: ''
+      },
+      time: {
+        weeks: 0,
+        days: 0,
+        hours: 0
       }
     }
+  }
+
+  // Update the time state based on a form change
+  changeTime(property, value){
+    let timeString = ''
+    let time = this.state.time
+    time[property] = value
+    if (time.weeks) timeString += time.weeks+' Weeks '
+    if (time.days) timeString += time.days+' Days '
+    if (time.hours) timeString += time.hours+' Hours '
+    this.handleChangeOn('time', timeString)
   }
 
   // Update the state based on a form change
@@ -31,12 +45,14 @@ class Options extends Component {
 
   render(){
 
-    const label = {
+    const fullWidth = {
       "width": "100%"
     }
 
-    const input = {
-      "width": "100%"
+    const timeInput = {
+      "width": "20%",
+      "display": "inline-block",
+      "marginRight": "10px"
     }
 
     return (
@@ -46,21 +62,21 @@ class Options extends Component {
         </div>
         <span></span>
         <div className="modal fade" id="newTask" role="dialog">
-          <div className="taskDetails">
+          <div className="modal-panel">
             <div className="modal-header">
               <button type="button" className="close" data-dismiss="modal">&times;</button>
               <h4 className="modal-title">New Task</h4>
             </div>
             <div className="modal-body">
-              <label style={label} className="form-group">
+              <label style={fullWidth} className="form-group">
                 Name
-                <input style={input} className="form-control" type="text" value={this.state.newTask.name} onChange={(event) => this.handleChangeOn('name', event.target.value)} />
+                <input style={fullWidth} className="form-control" type="text" value={this.state.newTask.name} onChange={(event) => this.handleChangeOn('name', event.target.value)} />
               </label>
-              <label style={label} className="form-group">
+              <label style={fullWidth} className="form-group">
                 Description
-                <textarea style={input} className="form-control" rows="6" value={this.state.newTask.description} onChange={(event) => this.handleChangeOn('description', event.target.value)}></textarea>
+                <textarea style={fullWidth} className="form-control" rows="6" value={this.state.newTask.description} onChange={(event) => this.handleChangeOn('description', event.target.value)}></textarea>
               </label>
-              <label style={label} className="form-group">
+              <label style={fullWidth} className="form-group">
                 Priotity
                 <select className="form-control" id="sel1" value={this.state.newTask.priority} onChange={(event) => this.handleChangeOn('priority', event.target.value)}>
                   <option>Ultra Low</option>
@@ -70,14 +86,22 @@ class Options extends Component {
                   <option>Ultra High</option>
                 </select>
               </label>
+              <label style={timeInput} className="form-group">
+                Weeks
+                <input className="form-control" type="number" min="0" max="2" value={this.state.time.weeks} onChange={(event) => this.changeTime('weeks', event.target.value)} />
+              </label>
+              <label style={timeInput} className="form-group">
+                Days
+                <input className="form-control" type="number" min="0" max="7" value={this.state.time.days} onChange={(event) => this.changeTime('days', event.target.value)} />
+              </label>
+              <label style={timeInput} className="form-group">
+                Hours
+                <input className="form-control" type="number" min="0" max="8" value={this.state.time.hours} onChange={(event) => this.changeTime('hours', event.target.value)} />
+              </label>
             </div>
             <div className="modal-footer">
-              <div className="col-md-3">
-                <button type="button" className="btn btn-neutral" data-dismiss="modal" onClick={this.addTask}>Create</button>
-              </div>
-              <div className="col-md-3">
-                <button type="button" className="btn btn-neutral" data-dismiss="modal">Close</button>
-              </div>
+              <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.addTask}>Create</button>
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Task.css'
 
 const Task = (props) => {
   // Set shorthand to current task
@@ -7,36 +8,9 @@ const Task = (props) => {
   let currentStage = props.stages.find((stage) => stage.id === props.task.stage_id)
 
   // Task card's local styles
-  const name = {
-    "fontSize": "18px",
-    "padding": "5px",
-    "backgroundColor": "rgb(221, 221, 221)"
-  }
-
-  const description = {
-    "maxHeight": "100px",
-    "textOverflow": "ellipsis",
-    "overflow": "hidden",
-    "padding": "5px"
-  }
-
-  const time = {
-    "textTransform": "uppercase",
-    "padding": "5px"
-  }
-
-  const priority = {
-    "textTransform": "uppercase",
-    "padding": "5px"
-  }
-
   const modalHeader = {
     "backgroundColor": currentStage.color,
     "color": "white"
-  }
-
-  const modalFooter = {
-    "textAlign": "left"
   }
 
   const button = {
@@ -58,37 +32,39 @@ const Task = (props) => {
   return (
     <div>
       <div className="col-md-12 card" data-toggle="modal" data-target={"#task"+task.id}>
-        <p style={name}>{task.name}</p>
-        <p style={description}>{task.description}</p>
+        <p className="name">{task.name}</p>
+        <p className="description">{task.description}</p>
         <div className="col-md-6">
-          <p style={time}>{task.time}</p>
+          <p className="task-info">{task.time}</p>
         </div>
         <div className="col-md-6">
-          <p style={priority}>{task.priority}</p>
+          <p className="task-info">{task.priority} priority</p>
         </div>
       </div>
 
       <div className="modal fade" id={"task"+task.id} role="dialog">
-        <div className="taskDetails">
+        <div className="modal-panel">
           <div className="modal-header" style={modalHeader}>
             <button type="button" className="close" data-dismiss="modal">&times;</button>
             <h4 className="modal-title">{task.name}</h4>
           </div>
           <div className="modal-body">
-            <p>{task.description}</p>
-            <p>Duration: <span style={time}>{task.time}</span></p>
-            <p>Priority: <span style={priority}>{task.priority}</span></p>
+            <p className="details">{task.description}</p>
+            <p>Duration: <span className="task-info">{task.time}</span></p>
+            <p>Priority: <span className="task-info">{task.priority}</span></p>
           </div>
-          <div className="modal-footer" style={modalFooter}>
+          <div className="modal-footer">
             <div className="col-md-12">
-              <button type="button" className="btn btn-danger" style={button} data-dismiss="modal" onClick={() => props.remove(task.id)}>Delete</button>
-              <button type="button" className="btn btn-neutral dropdown-toggle" style={button} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {currentStage.name}
+              <button type="button" className="btn btn-danger" style={button} data-dismiss="modal" onClick={() => props.remove(task.id)}>
+                Delete
+              </button>
+              <button type="button" className="btn btn-default dropdown-toggle" style={button} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Move
               </button>
               <div className="dropdown-menu">
                 {dropdownOptions}
               </div>
-              <button type="button" className="btn btn-neutral" style={button} data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-default" style={button} data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
